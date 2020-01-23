ac23288f929a2e9f40b40408bb35fac16b4c9870
larrygf02 <raulgf_02@hotmail.com>
2020-01-23 00:21:02 -0500

    Hash key

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,20 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl)
-client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
 //almacenando una copia exacta de la funcion exec de mongoose
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '')
+    return this;
+}
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
     console.log('IM ABOUT TO RUN A QUERY');
 /*     console.log(this.getQuery());
     console.log(this.mongooseCollection.name); */
@@ -28,6 +37,12 @@ mongoose.Query.prototype.exec = async function () {
     }
 
     const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result))
+    client.hset(this.hashKey ,key, JSON.stringify(result), 'EX', 10)
     return result;
+}
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey))
+    }
 }
