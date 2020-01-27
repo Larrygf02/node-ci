2d1b5f228142f8919ba546f9dd09e4abb8093969
larrygf02 <raulgf_02@hotmail.com>
2020-01-26 21:54:13 -0500

    Click url test

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 
 let browser, page;
 
+// antes de cada prueba
 beforeEach( async () => {
     browser = await puppeteer.launch({
         headless: false
@@ -10,8 +11,19 @@ beforeEach( async () => {
     await page.goto('localhost:3000')
 })
 
+// despues de cada prueba
+afterEach(async () => {
+    await browser.close()
+})
+
 test('We can launch a browser', async () => {
     // Logo de la pagina 
     const text = await page.$eval('a.brand-logo', el => el.innerHTML)
     expect(text).toEqual('Blogster');
 });
+
+test('clicking login', async () => {
+    await page.click('.right a');
+    const url = await page.url()
+    console.log(url);
+})
