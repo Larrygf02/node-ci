db21bc621c09afc4b23e116df095098156d2339d
larrygf02 <raulgf_02@hotmail.com>
2020-02-06 22:42:04 -0500

    refactory

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory')
+const userFactory = require('./factories/userFactory')
 
 let browser, page;
 
@@ -22,29 +24,21 @@ test('We can launch a browser', async () => {
     expect(text).toEqual('Blogster');
 });
 
-/* test('clicking login', async () => {
+test('clicking login', async () => {
     await page.click('.right a');
     const url = await page.url()
     expect(url).toMatch(/accounts\.google\.com/);
-}) */
+})
 
 test.only('when signed in, shows logout button', async () => {
-    const id = '5dbc53fd9aa9504d9caec4b4';
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    }
+    const user = await userFactory()
+    const { session, sig } = sessionFactory(user)
 
-    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
-    const Keygrip = require('keygrip')
-    const keys = require('../config/keys')
-    const keygrip = new Keygrip([keys.cookieKey])
-    const sig = keygrip.sign('session=' + sessionString);
-    console.log(sessionString, sig);
-
-    await page.setCookie({ name: 'session', value: sessionString})
+    await page.setCookie({ name: 'session', value: session})
     await page.setCookie({ name: 'session.sig', value: sig})
     await page.goto('localhost:3000')
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(text).toEqual('Logout');
 })
