7dd199ed8483724c87b5bcecf3a5ed0227da7c7c
larrygf02 <raulgf_02@hotmail.com>
2020-02-09 12:12:16 -0500

    config keys travis

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,8 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   //mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  mongoURI: 'mongodb://raulgf:blWzBslJNzI7BVa9@cluster0-shard-00-00-caana.mongodb.net:27017,cluster0-shard-00-01-caana.mongodb.net:27017,cluster0-shard-00-02-caana.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123'
+  // mongoURI: 'mongodb://raulgf:blWzBslJNzI7BVa9@cluster0-shard-00-00-caana.mongodb.net:27017,cluster0-shard-00-01-caana.mongodb.net:27017,cluster0-shard-00-02-caana.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
